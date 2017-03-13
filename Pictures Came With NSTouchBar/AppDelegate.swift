2350613299f8e235b9c4a38d84de6be0d86aa87a
Austin Marshall <amarshall@numenta.com>
2017-03-13 13:23:34 -0700

    Initial Commit

---
+++ Pictures Came With NSTouchBar/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Pictures Came With NSTouchBar
+//
+//  Created by Austin Marshall on 3/13/17.
+//  Copyright © 2017 Austin Marshall. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
