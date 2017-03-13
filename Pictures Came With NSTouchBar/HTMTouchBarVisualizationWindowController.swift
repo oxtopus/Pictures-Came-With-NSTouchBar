82a0c4823898b10407ffe2aa962a36e3a78255a2
Austin Marshall <amarshall@numenta.com>
2017-03-13 13:29:52 -0700

    First commit!

---
+++ Pictures Came With NSTouchBar/HTMTouchBarVisualizationWindowController.swift
@@ -0,0 +1,20 @@
+//
+//  HTMTouchBarVisualizationWindowController.swift
+//  Pictures Came With NSTouchBar
+//
+//  Created by Austin Marshall on 3/13/17.
+//  Copyright © 2017 Austin Marshall. All rights reserved.
+//
+
+import Cocoa
+
+class HTMTouchBarVisualizationWindowController: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+    
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+        print("Hello.")
+    }
+
+}
